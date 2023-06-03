a73c304d50d3646941641ee853da0dc63c8d3225
fabio <fabio@host.docker.internal>
2023-06-03 19:49:20 -0300

    feat: create table paciente sql file

---
+++ api/src/main/resources/db/migration/V5__create-table-pacientes.sql
@@ -0,0 +1,15 @@
+CREATE TABLE pacientes (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(100) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  telefone VARCHAR(16) NOT NULL,
+  cpf CHAR(11) NOT NULL,
+  logradouro VARCHAR(100) NOT NULL,
+  bairro VARCHAR(100) NOT NULL,
+  cep VARCHAR(9) NOT NULL,
+  complemento VARCHAR(100),
+  numero VARCHAR(100),
+  uf CHAR(2) NOT NULL,
+  cidade VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
